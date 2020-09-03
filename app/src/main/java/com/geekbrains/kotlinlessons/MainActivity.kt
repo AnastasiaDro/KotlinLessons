1fe1ef89da9bb02c71c73d27f3b36db7fae54066
AnastasiaDro <adrogunova@gmail.com>
2020-09-03 10:55:52 +0300

    - теперь по нажатию на кнопку появляется всплывающее сообщение

--- app/src/main/java/com/geekbrains/kotlinlessons/MainActivity.kt
+++ app/src/main/java/com/geekbrains/kotlinlessons/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.geekbrains.kotlinlessons
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //текст всплывающего сообщения
+    val msgText = "Hello world!"
+    val duration = Toast.LENGTH_LONG
+ //   val toast = Toast.makeText(applicationContext, msgText, duration)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //ClickListener для кнопки
         button.setOnClickListener {
-
+            val toast = Toast.makeText(applicationContext, msgText, duration)
+            toast.show()
         }
 
         }
